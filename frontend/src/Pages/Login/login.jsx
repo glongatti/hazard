c24435339f69031f4f2000e05458cd818bbffc93
Gabriel <gabriel.augusto007@gmail.com>
2019-05-03 20:58:35 -0300

    - Finalizada funcionalidade de login do usuário

--- frontend/src/Pages/Login/login.jsx
+++ frontend/src/Pages/Login/login.jsx
@@ -1,18 +1,76 @@
 import React from 'react';
 
 import {
-    Form, Icon, Input, Button
+    Form, Icon, Input, Button, Alert
 } from 'antd';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+
+import axios from 'axios';
 
 import "./../Login/login.css"
+import { appConfig } from './../../config'
+
 class Login extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            userAuthenticated: false,
+            hasRequestError: false,
+            errorMsg: ''
+        }
+    }
+
+    async loginRequest(email, password) {
+        var headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "X-Requested-With"
+        }
+
+        return axios.post(`http://127.0.0.1:8090/usuario/login?usuario=${email}&senha=${password}`, null, headers)
+    }
 
     handleSubmit = (e) => {
         e.preventDefault();
+        let self = this;
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+                // vai fazer request axios para logar
+                this.loginRequest(values['email'], values['password']).then((response) => {
+                    console.log(response)
+
+                    if (response.status == 200) {
+                        const userObject = {
+                            id: response.data.body.id,
+                            email: response.data.body.email,
+                            name: response.data.body.nome
+                        }
+
+                        localStorage.setItem('user', JSON.stringify(userObject))
+                        self.setState({
+                            userAuthenticated: true
+                        })
+
+                    }
+                }).catch((err) => {
+                    if(err.response){
+                        self.setState({
+                            hasRequestError:true,
+                            errorMsg:err.response.data.error
+                        })
+
+                    }else{
+                        self.setState({
+                            hasRequestError:true,
+                            errorMsg:"Erro Interno no Servidor, por favor tente novamente mais tarde!"
+                        })
+
+                    }
+                    
+                })
+
             }
         });
     }
@@ -21,20 +79,25 @@ class Login extends React.Component {
     render() {
         const { getFieldDecorator } = this.props.form;
 
+        if (this.state.userAuthenticated) return <Redirect to={"/meus-alertas"} />;
+
         return (
             <div>
-                <h1 class={"loginTitle"}>Formulário de Login</h1>
-                <div class={"divLogin"}>
+                <h1 className={"loginTitle"}>Formulário de Login</h1>
+                <div className={"divLogin"}>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
-                            {getFieldDecorator('email', {
-                                rules: [{
-                                    type: 'email', message: 'O e-mail inserido não é válido!',
-                                },
-                                { required: true, message: 'Por favor insira seu e-mail!' }],
-                            })(
-                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="E-mail" />
-                            )}
+                            {getFieldDecorator('email',
+                                {
+                                    rules: [
+                                        //     {
+                                        //     type: 'email', message: 'O e-mail inserido não é válido!',
+                                        // },
+                                        { required: true, message: 'Por favor insira seu e-mail!' }],
+                                    initialValue: this.state.email,
+                                })(
+                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="E-mail" />
+                                )}
                         </Form.Item>
                         <Form.Item>
                             {getFieldDecorator('password', {
@@ -48,10 +111,14 @@ class Login extends React.Component {
                                 Fazer Login!
                         </Button>
                             &nbsp;
-                        Or &nbsp; <Link to="">Quero me cadastrar!</Link>
+                        Or &nbsp; <Link to="/cadastro">Quero me cadastrar!</Link>
                         </Form.Item>
                     </Form>
                 </div>
+                {this.state.hasRequestError && (
+                    <Alert style={{ margin: ' 20px auto', maxWidth: 500 }} type={"error"} message={"Erro!"} description={this.state.errorMsg} />
+                )}
+
             </div>
         );
     }

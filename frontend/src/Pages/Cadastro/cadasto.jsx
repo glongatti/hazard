0bb2d11bf87e3954b7167e04f5dc390722e2249b
Gabriel <gabriel.augusto007@gmail.com>
2019-05-03 21:36:39 -0300

    Finalizado funcionalidade de cadastro de usuário

--- frontend/src/Pages/Cadastro/cadasto.jsx
+++ frontend/src/Pages/Cadastro/cadasto.jsx
@@ -1,17 +1,80 @@
 import React from 'react';
 
 import {
-    Form, Icon, Input, Button
+    Form, Icon, Input, Button, Alert
 } from 'antd';
+import axios from 'axios';
+import { Link, Redirect } from "react-router-dom";
+
 
 import "./../Cadastro/cadastro.css"
 class Cadastro extends React.Component {
 
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            userAuthenticated: false,
+            hasRequestError: false,
+            errorMsg: ''
+        }
+    }
+
+    async registerRequest(obj) {
+        var headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "X-Requested-With"
+        }
+
+        return axios.post(`http://127.0.0.1:8090/usuario`, obj, headers)
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
+        let self = this;
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+
+                var jsonObject = {
+                    "nome": values["nome"],
+                    "email": values["email"],
+                    "senha": values["senha"]
+                }
+
+                // vai fazer request axios para logar
+                this.registerRequest(jsonObject).then((response) => {
+                    if (response.status == 200) {
+
+                        const userObject = {
+                            id: response.data.id,
+                            email: response.data.email,
+                            name: response.data.nome
+                        }
+
+                        localStorage.setItem('user', JSON.stringify(userObject))
+                        self.setState({
+                            userAuthenticated: true
+                        })
+
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                    if (err.response) {
+                        self.setState({
+                            hasRequestError: true,
+                            errorMsg: err.response.data.error
+                        })
+
+                    } else {
+                        self.setState({
+                            hasRequestError: true,
+                            errorMsg: "Erro Interno no Servidor, por favor tente novamente mais tarde!"
+                        })
+
+                    }
+
+                })
+
             }
         });
     }
@@ -20,10 +83,12 @@ class Cadastro extends React.Component {
     render() {
         const { getFieldDecorator } = this.props.form;
 
+        if (this.state.userAuthenticated) return <Redirect to={"/meus-alertas"} />;
+
         return (
             <div>
-                <h1 class={"loginTitle"}>Formulário de Cadastro</h1>
-                <div class={"divLogin"}>
+                <h1 className={"loginTitle"}>Formulário de Cadastro</h1>
+                <div className={"divLogin"}>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <p>Por favor, preencha os campos abaixo para poder fazer o seu cadastro</p>
                         <Form.Item>
@@ -44,7 +109,7 @@ class Cadastro extends React.Component {
                             )}
                         </Form.Item>
                         <Form.Item>
-                            {getFieldDecorator('password', {
+                            {getFieldDecorator('senha', {
                                 rules: [{ required: true, message: 'Preencha sua senha por favor!' }],
                             })(
                                 <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
@@ -57,6 +122,9 @@ class Cadastro extends React.Component {
                         </Form.Item>
                     </Form>
                 </div>
+                {this.state.hasRequestError && (
+                    <Alert style={{ margin: ' 20px auto', maxWidth: 500 }} type={"error"} message={"Erro!"} description={this.state.errorMsg} />
+                )}
             </div>
         );
     }

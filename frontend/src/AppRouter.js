0edcc92580e552918459233ffaa269046b2de313
Gabriel <gabriel.augusto007@gmail.com>
2019-05-03 21:02:11 -0300

    Alterações approuter e package.json

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -10,20 +10,48 @@ import Login from './Pages/Login/login'
 import Cadastro from './Pages/Cadastro/cadasto'
 import MeusAlertas from './Pages/MeusAlertas/meus-alertas';
 import Alertas from './Pages/Alertas/alertas'
+import CadastroAlerta from './Pages/CadastroAlerta/cadastro-alerta'
 
 class AppRouter extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLogged: false
+    }
+  }
+
+  componentWillMount() {
+
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    if (user) {
+      console.log('nvarbar ta logado')
+      this.setState({
+        isLogged: true,
+      })
+    } else {
+      console.log('nvarbar naooo ta logado')
+      this.setState({
+        isLogged: false,
+      })
+    }
+  }
+
   render() {
+    console.log('renderizo')
     return (
       <div className="App">
 
 
         <Router Navbar={Navbar}>
-          <Navbar />
+          <Navbar isLogged={this.state.isLogged} />
           <Route path="/" exact component={Index} />
           <Route path="/alertas" exact component={Alertas} />
           <Route path="/login" component={Login} />
           <Route path="/cadastro" component={Cadastro} />
           <Route path="/meus-alertas" component={MeusAlertas} />
+          <Route path="/cadastro-alerta" component={CadastroAlerta} />
         </Router>
 
       </div>
